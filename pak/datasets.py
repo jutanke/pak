c5f7e3eb41110be326e0fd1669e2f4ccfe71b137
root <jutanke@googlemail.com>
2017-11-11 11:47:42 +0000

    module system now works + testing

--- pak/datasets.py
+++ pak/datasets.py
@@ -1,2 +1,13 @@
+# Find and download typical datasets for deep learning
+#
+from pak import utils
+
 def test():
-    print("lol")
+    utils.talk("LOOL", True)
+    print('lol')
+
+
+def get2DMOT2015():
+    """ Gets the 2d MOT2015 dataset
+    """
+    pass
--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-#from distutils.core import setup
-
-#setup_req = ['numpy']
-#install_req = ['numpy']
-
-#setup(
-#    name = 'pak',
-#    packages = ['pak'],
-#    install_requires=install_req,
-#    setup_requires=setup_req,
-#    version = '0.0.1',
-#    description = 'General Dataset Helper Functions',
-#    author = 'Julian Tanke',
-#    url='https://github.com/justayak/pppr'
-#)
-
-#from setuptools import setup, find_packages
-
-#print("PACKAGES:",find_packages())
-
-#setup(
-#    name="pak",
-#    version="0.0.3",
-#    packages=find_packages(),
-#)
-
 from distutils.core import setup
 
 setup(
