9bfd490812cb65e6c10de2cab8ec35b043426fb8
root <jutanke@googlemail.com>
2017-11-12 17:16:31 +0000

    add mot16

--- pak/datasets.py
+++ pak/datasets.py
@@ -1,6 +1,54 @@
 # Find and download typical datasets for deep learning
 #
+import zipfile
+import urllib.request
+import shutil
+from os import makedirs
+from os.path import join, isfile, exists
 from pak import utils
+
+
+class Dataset:
+    """ Dataset base class
+    """
+
+    def __init__(self, name, root, verbose=True):
+        self.name = name
+        self.root = root
+        self.verbose = verbose
+        if not exists(root):
+            makedirs(root)
+
+
+    def download_and_unzip(self, url):
+        """ Downloads and unzips a zipped data file
+
+        """
+        dest = join(self.root, self.name)
+        if not exists(dest):
+            utils.talk("could not find folder " + dest + "...", self.verbose)
+            fzip = join(self.root, self.name + ".zip")
+
+            if not isfile(fzip):
+                utils.talk("could not find file " + fzip, self.verbose)
+                with urllib.request.urlopen(url) as res, open(fzip, 'wb') as f:
+                    shutil.copyfileobj(res, f)
+                zip_ref = zipfile.ZipFile(fzip, 'r')
+                zip_ref.extractall(self.root)
+                zip_ref.close()
+
+
+# =========================================
+#  MOT16
+# =========================================
+
+class MOT16(Dataset):
+
+    def __init__(self, root, verbose=True):
+        Dataset.__init__(self, "MOT16", root, verbose)
+        url = 'https://motchallenge.net/data/MOT16.zip'
+        self.download_and_unzip(url)
+
 
 def test():
     utils.talk("LOOL", True)
--- pak/utils.py
+++ pak/utils.py
@@ -3,20 +3,10 @@ import shutil
 import zipfile
 from os.path import join, isfile, exists
 
+
 def talk(text, verbose):
     """ helper function for printing debug messages
     """
     if verbose:
         print(str(text))
-
-def download_and_unzip(dest, fzip, url, verbose=False):
-    """ Downloads and unzips a zipped data file
-
-    dest: {String}, folder that contains the unzipped data
-    fzip: {String}, file name of the zipped downloaded file
-    url: {String}, URL for the zip file
-    """
-    if exists(dest):
-        talk(dest + " exists, returning..", verbose)
-
 
