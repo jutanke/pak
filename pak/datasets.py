bfa16f7256d14f48f28d3c453bac380917251c64
Julian <jutanke@googlemail.com>
2017-11-14 14:52:46 +0100

    add Dataset Market 1501

--- pak/datasets.py
+++ pak/datasets.py
@@ -1,6 +1,6 @@
 # Find and download typical datasets for deep learning
 
-import numpy as np#
+import numpy as np
 import zipfile
 import urllib.request
 import shutil
@@ -43,7 +43,9 @@ class Dataset:
             utils.talk("could not find folder " + dest + "...", self.verbose)
             fzip = join(self.root, self.name + ".zip")
 
-            if not isfile(fzip):
+            if isfile(fzip):
+                utils.talk('found ' + fzip)
+            else:
                 utils.talk("could not find file " + fzip, self.verbose)
                 utils.talk("download from " + url, self.verbose)
                 with urllib.request.urlopen(url) as res, open(fzip, 'wb') as f:
@@ -54,7 +56,7 @@ class Dataset:
             zip_ref.extractall(self.root_export)
             zip_ref.close()
         else:
-            utils.talk(self.root_export + ' found :)', self.verbose)
+            utils.talk(dest + ' found :)', self.verbose)
 
 
 # =========================================
@@ -92,10 +94,10 @@ class MOT_X(Dataset):
                 "Occluder full",
                 "Reflection" ][int(label_id)]
 
-    def get_train_raw(self, folder):
+    def get_raw(self, folder, parent):
         """ get the raw train data
         """
-        root = join(self.root_export, "train")
+        root = join(self.root_export, parent)
         loc = join(root, folder)
 
         # X
@@ -106,17 +108,33 @@ class MOT_X(Dataset):
             X = np.array([imread(f) for f in imgs], 'uint8')
         else:
             X = np.array([imresize(imread(f), size=self.resize) for f in imgs], 'uint8')
-        utils.talk('MOT16 X loaded', self.verbose)
+        utils.talk(self.name + ' X loaded', self.verbose)
 
         # Y-det
         det_txt = join(join(loc, "det"), 'det.txt')
         Y_det = np.loadtxt(det_txt, delimiter=',')
-        utils.talk('MOT16 Y_det loaded', self.verbose)
+        utils.talk(self.name + ' Y_det loaded', self.verbose)
+
+        return X, Y_det
+
+    def get_test_raw(self, folder):
+        """ Gets the raw MOT data for testing
+        """
+        parent = 'test'
+        return MOT_X.get_raw(self, folder, parent=parent)
+
+    def get_train_raw(self, folder):
+        """ Gets the raw MOT data for training
+        """
+        parent = 'train'
+        X, Y_det = MOT_X.get_raw(self, folder, parent=parent)
 
         # Y-gt
+        root = join(self.root_export, parent)
+        loc = join(root, folder)
         gt_txt = join(join(loc, "gt"), 'gt.txt')
         Y_gt = np.loadtxt(gt_txt, delimiter=',')
-        utils.talk('MOT16 Y_gt loaded', self.verbose)
+        utils.talk(self.name + ' Y_gt loaded', self.verbose)
 
         return X, Y_det, Y_gt
 
@@ -134,7 +152,7 @@ class MOT16(MOT_X):
     """
 
     def __init__(self, root, verbose=True, resize=None):
-        root_export = join(root, "MOT16")
+        root_export = join(root, "MOT16")  # force dir name for unzipping
         MOT_X.__init__(self, root, root_export, "MOT16",
                 "https://motchallenge.net/data/MOT16.zip", verbose, resize)
 
@@ -154,6 +172,7 @@ class MOT152D(MOT_X):
     def __init__(self, root, verbose=True, resize=None):
         MOT_X.__init__(self, root, root, "2DMOT2015",
                 "https://motchallenge.net/data/2DMOT2015.zip", verbose, resize)
+        self.root_export = join(root, "2DMOT2015")  # the dirs name after unzip
 
 
 # =========================================
@@ -164,8 +183,40 @@ class Market1501(Dataset):
     """ Market1501 dataset
     """
 
-    def __init__(self, root):
+    def __init__(self, root, verbose=True):
         Dataset.__init__(self, "Market-1501-v15.09.15", root, verbose)
+        self.download_and_unzip('NOT-YET-THERE')
+        self.root_export = join(root, "Market-1501-v15.09.15")
+
+    def get_train_raw(self):
+        return self.get_raw('bounding_box_train')
+
+    def get_test_raw(self):
+        return self.get_raw('bounding_box_test')
+
+    def get_raw(self, folder):
+        """ gets the raw identity pictures
+        """
+        loc = join(self.root_export, folder)
+        imgs = sorted([f \
+                    for f in listdir(loc) if isfile(join(loc, f)) and \
+                      f.endswith('jpg')])
+        X = np.array([imread(join(loc, f)) for f in imgs], 'uint8')
+
+        identities = np.array([int(f[0:2]) if f.startswith('-1') else int(f[0:4]) \
+            for f in imgs])
+        cameras = np.array([int(f[4]) if f.startswith('-1') else int(f[6]) \
+            for f in imgs])
+        sequences = np.array([int(f[6]) if f.startswith('-1') else int(f[8]) \
+            for f in imgs])
+        frames = np.array([int(f[8:14]) if f.startswith('-1') else int(f[10:16]) \
+            for f in imgs])
+
+        Y = np.vstack((identities, cameras, sequences, frames))
+
+        return X, Y
+
+
 
 # =========================================
 #  CUHK03
