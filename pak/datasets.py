028703f316a70a5e5db190465c065294b7417fbc
Julian <jutanke@googlemail.com>
2017-11-16 21:41:42 +0000

    add lspe

--- pak/datasets.py
+++ pak/datasets.py
@@ -8,6 +8,7 @@ from os import makedirs, listdir
 from os.path import join, isfile, exists
 from scipy.ndimage import imread
 from scipy.misc import imresize
+from scipy.io import loadmat
 from skimage.transform import resize
 from pak import utils
 import h5py
@@ -52,7 +53,7 @@ class Dataset:
             fzip = join(self.root, self.name + ".zip")
 
             if isfile(fzip):
-                utils.talk('found ' + fzip)
+                utils.talk('found ' + fzip, self.verbose)
             else:
                 utils.talk("could not find file " + fzip, self.verbose)
                 utils.talk("download from " + url, self.verbose)
@@ -235,6 +236,34 @@ class Market1501(Dataset):
         return X, np.rollaxis(Y, 1)
 
 
+# =========================================
+#  LSPE
+# =========================================
+class LSPE(Dataset):
+    """ Leeds Sports Pose Extended
+    """
+
+    def __init__(self, root, verbose=True):
+        Dataset.__init__(self, 'lspe', root, verbose)
+        url = 'http://sam.johnson.io/research/lspet_dataset.zip'
+        self.root_export = join(root, "lspe")
+        self.download_and_unzip(url)
+
+    def get_raw(self):
+        """ gets the raw data without any resizing.
+            Attention: Image sizes vary!
+        """
+        image_folder = join(self.root_export, 'images')
+        joint_mat = join(self.root_export, 'joints.mat')
+
+        Y = np.rollaxis(loadmat(joint_mat)['joints'], 2, 0)
+        n, _, _ = Y.shape
+
+        imgs = sorted([f for f in listdir(image_folder) if \
+                        isfile(join(image_folder, f))])
+        X = np.array([imread(join(image_folder, f)) for f in imgs])
+
+        return X, Y
 
 # =========================================
 #  CUHK03
