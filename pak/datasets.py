c9bd9f124eb96f01e16e6502343b75ff6fa6e525
Julian <jutanke@googlemail.com>
2017-11-22 15:41:45 +0100

    add dukemtmc-reid dataset

--- pak/datasets.py
+++ pak/datasets.py
@@ -266,6 +266,42 @@ class LSPE(Dataset):
         return X, Y
 
 # =========================================
+#  DukeMTMC-reID
+# =========================================
+
+class DukeMTMC_reID(Dataset):
+    """ DukeMTMC-reID dataset, very similar to Market1501
+    """
+
+    def __init__(self, root, verbose=True):
+        Dataset.__init__(self, "DukeMTMC-reID", root, verbose)
+        Dataset.unzip(self)
+        self.root_export = join(root, "DukeMTMC-reID")
+
+    def get_train(self):
+        return self.get_raw('bounding_box_train')
+
+    def get_test(self):
+        return self.get_raw('bounding_box_test')
+
+    def get_raw(self, folder):
+        """ gets the raw identity pictures
+        """
+        loc = join(self.root_export, folder)
+        imgs = sorted([f \
+                    for f in listdir(loc) if isfile(join(loc, f)) and \
+                      f.endswith('jpg')])
+        X = np.array([imread(join(loc, f)) for f in imgs])
+
+        identities = np.array([int(f[0:4]) for f in imgs])
+        cameras = np.array([int(f[6]) for f in imgs])
+        frames = np.array([int(f[9:15]) for f in imgs])
+
+        Y = np.vstack((identities, cameras, frames))
+
+        return X, np.rollaxis(Y, 1)
+
+# =========================================
 #  CUHK03
 # =========================================
 
