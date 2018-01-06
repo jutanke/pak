6ee1a43f1558c005af8d0e7a86fd00557c28ff0b
Julian <jutanke@googlemail.com>
2018-01-06 15:38:43 +0000

    add resizing and memmapping to Market1501

--- pak/datasets/Market1501.py
+++ pak/datasets/Market1501.py
@@ -13,6 +13,7 @@ from skimage.transform import resize
 from pak import utils
 from pak.util import mpii_human_pose as mpii_hp
 import h5py
+import time
 from enum import Enum
 
 # =========================================
@@ -23,26 +24,97 @@ class Market1501(Dataset):
     """ Market1501 dataset
     """
 
-    def __init__(self, root, verbose=True):
+    def __init__(self, root, verbose=True, force_shape=None, memmapped=True):
+        """
+        force_shape: (w,h) if set the images will be forced into a
+            given shape
+        """
         url = 'http://188.138.127.15:81/Datasets/Market-1501-v15.09.15.zip'
         Dataset.__init__(self, "Market-1501-v15.09.15", root, verbose)
         self.download_and_unzip(url)
         self.root_export = join(root, "Market-1501-v15.09.15")
+        self.memmapped = memmapped
+
+        if force_shape is None:
+            self.force_shape = None
+        else:
+            assert len(force_shape) == 2
+            w,h = force_shape
+            self.force_shape = (h, w)  # as resize used inversed logic..
+
+
+    def get_memmapped_file_name(self, folder):
+        shape_str = ''
+        force_shape = self.force_shape
+        if force_shape is not None:
+            shape_str = str(force_shape[0]) + 'x' + str(force_shape[1])
+
+        file_name = folder + shape_str  + '.npy'
+        return join(self.root_export , file_name)
+
+
+    def get_memmapped_file_shape(self, folder):
+        if folder == 'bounding_box_train':
+            n = 12936
+        elif folder == 'bounding_box_test':
+            n = 19732
+        else:
+            raise Exception("Cannot find subsystem " + folder)
+
+        force_shape = self.force_shape
+        if force_shape is None:
+            h,w = 128,64
+        else:
+            h,w = force_shape
+
+        return (n, h, w, 3)
+
 
     def get_train(self):
         return self.get_raw('bounding_box_train')
 
+
     def get_test(self):
         return self.get_raw('bounding_box_test')
+
 
     def get_raw(self, folder):
         """ gets the raw identity pictures
         """
+
+        X_is_loaded = False
+        if self.memmapped:
+            fmmap = self.get_memmapped_file_name(folder)
+            data_shape = self.get_memmapped_file_shape(folder)
+            if isfile(fmmap):
+                X = np.memmap(fmmap, dtype='uint8', mode="r", shape=data_shape)
+                X_is_loaded = True
+
+
         loc = join(self.root_export, folder)
-        imgs = sorted([f \
-                    for f in listdir(loc) if isfile(join(loc, f)) and \
-                      f.endswith('jpg')])
-        X = np.array([imread(join(loc, f)) for f in imgs], 'uint8')
+
+        imgs = sorted([f for f in listdir(loc) if isfile(join(loc, f)) and \
+                    f.endswith('jpg')])
+        if not X_is_loaded:
+            force_shape = self.force_shape
+
+            if force_shape is None:
+                X = np.array([imread(join(loc, f)) for f in imgs], 'uint8')
+            else:
+                X = np.array([imresize(imread(join(loc, f)), size=force_shape) \
+                    for f in imgs], 'uint8')
+
+            if self.memmapped:
+                # store to memmap
+                X_ = np.memmap(fmmap, dtype='uint8', mode="w+", shape=data_shape)
+                for i, x in enumerate(X):
+                    X_[i] = x
+
+                utils.talk('flush memmaped ' + fmmap, self.verbose)
+                del X_  # flush
+                time.sleep(3)
+
+                X = np.memmap(fmmap, dtype='uint8', mode="r", shape=data_shape)
 
         identities = np.array([int(f[0:2]) if f.startswith('-1') else int(f[0:4]) \
             for f in imgs])
@@ -56,3 +128,13 @@ class Market1501(Dataset):
         Y = np.vstack((identities, cameras, sequences, frames))
 
         return X, np.rollaxis(Y, 1)
+
+    # ----------------------------------------------
+    # static methods
+    # ----------------------------------------------
+
+    @staticmethod
+    def extract_ids(Y):
+        """ converts the Y-values into only id values
+        """
+        return Y[:,0]
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 setup(
     name="pak",
-    version="0.0.15",
+    version="0.0.16",
     packages=["pak", "pak/util", "pak/datasets", "pak/evaluation"],
 )
