b7a09b3e10e6a2e1b3b6fe4bdff75d06dc80cf1d
Julian <jutanke@googlemail.com>
2018-01-06 15:07:19 +0000

    make CUHK03 memorymapped

--- pak/datasets/CUHK03.py
+++ pak/datasets/CUHK03.py
@@ -4,6 +4,7 @@ import zipfile
 import tarfile
 import urllib.request
 import shutil
+import time
 from os import makedirs, listdir
 from os.path import join, isfile, isdir, exists, splitext
 from scipy.ndimage import imread
@@ -20,7 +21,7 @@ class cuhk03(Dataset):
     """ cuhk03 dataset
     """
 
-    def __init__(self, root, verbose=True, target_w=100, target_h=256):
+    def __init__(self, root, verbose=True, target_w=100, target_h=256, memmapped=True):
         """ create cuhk03 dataset
 
             root: data folder that stores all files
@@ -32,8 +33,37 @@ class cuhk03(Dataset):
         Dataset.__init__(self, "cuhk03_release", root, verbose)
         Dataset.unzip(self)
         self.hd5file = join(join(root, self.name), 'cuhk-03.mat')
-        self.target_w = target_w
-        self.target_h = target_h
+        self.memmapped_file_location = join(root, self.name)
+        self.target_w = int(target_w)
+        self.target_h = int(target_h)
+        self.memmapped = memmapped
+
+
+    def get_memmapped_file_name(self, folder):
+        """ type is either 'detected' or 'labeled'
+        """
+        file_name = folder + str(self.target_w) + "x" + str(self.target_h) +\
+            '.npy'
+        return join(self.memmapped_file_location, file_name)
+
+
+    def get_y_file(self, folder):
+        """ gives the filename for the y-data
+        """
+        file_name = folder + "_y.npy"
+        return join(self.memmapped_file_location, file_name)
+
+
+    def get_memmapped_file_shape(self, folder):
+        """ gives the right size
+        """
+        if folder == 'detected':
+            n = 14097
+        elif folder == 'labeled':
+            n = 14096
+        else:
+            raise Exception("Could not find shape for folder " + folder)
+        return (n, self.target_h, self.target_w, 3)
 
 
     def get_detected(self):
@@ -47,6 +77,28 @@ class cuhk03(Dataset):
         return self.get_raw('labeled')
 
     def get_raw(self, folder):
+
+        # step 1: check if we already have memmapped file:
+        X_is_loaded = False
+        if self.memmapped:
+            fmmap = self.get_memmapped_file_name(folder)
+            if isfile(fmmap):
+                utils.talk('found memmaped ' + fmmap, self.verbose)
+                data_shape = self.get_memmapped_file_shape(folder)
+                X = np.memmap(fmmap, dtype='uint8', mode="r", shape=data_shape)
+                X_is_loaded = True
+            else:
+                utils.talk('could not find memmaped ' + fmmap, self.verbose)
+
+        Y_is_loaded = False
+        fydata = self.get_y_file(folder)
+        if isfile(fydata):
+            Y = np.load(fydata)
+            Y_is_loaded = True
+
+        if X_is_loaded and Y_is_loaded:
+            return X, Y
+
         f = h5py.File(self.hd5file,'r+')
         data = f[folder]
         _, n = data.shape
@@ -67,12 +119,34 @@ class cuhk03(Dataset):
                 for j in range(ten):
                     img = f[V[j][i]].value
                     if len(img.shape) == 3:
-                        img = np.swapaxes(img, 0, 2)
-                        img = resize(img, (th, tw), mode='constant')
-                        Imgs.append((img * 255).astype('uint8'))
+                        if not X_is_loaded:
+                            img = np.swapaxes(img, 0, 2)
+                            img = resize(img, (th, tw), mode='constant')
+                            Imgs.append((img * 255).astype('uint8'))
                         Ids.append(current_id)
                 current_id += 1
 
-        X = np.array(Imgs)
+        if not X_is_loaded:
+            X = np.array(Imgs)
+            if self.memmapped:
+
+
+                utils.talk('write memmaped ' + fmmap, self.verbose)
+                data_shape = self.get_memmapped_file_shape(folder)
+                X_ = np.memmap(fmmap, dtype='uint8', mode="w+", shape=data_shape)
+                #X_[:] = X[:]
+                for i, x in enumerate(X):
+                    X_[i] = x
+
+                utils.talk('flush memmaped ' + fmmap, self.verbose)
+                del X_  # flush
+                time.sleep(3)
+
+                X = np.memmap(fmmap, dtype='uint8', mode="r", shape=data_shape)
+
         Y = np.array(Ids, 'int32')
+
+        if not Y_is_loaded:
+            np.save(fydata, Y)
+        
         return X, Y
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 setup(
     name="pak",
-    version="0.0.14",
+    version="0.0.15",
     packages=["pak", "pak/util", "pak/datasets", "pak/evaluation"],
 )
