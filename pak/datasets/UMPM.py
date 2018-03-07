0a3bd712080af7d47ad80c387b7354872b3a537b
Julian <jutanke@googlemail.com>
2018-03-07 23:14:19 +0100

    memmap UMPM

--- pak/datasets/UMPM.py
+++ pak/datasets/UMPM.py
@@ -4,6 +4,7 @@ from pak import utils
 from pak.util import download, unzip
 from os import makedirs, listdir
 from os.path import join, isfile, isdir, exists, splitext
+import cv2
 
 
 class UMPM:
@@ -17,6 +18,7 @@ class UMPM:
         utils.talk("UMPM", verbose)
 
         data_root = join(root, 'umpm')
+        self.data_root = data_root
         root_url = 'http://umpm-mirror.cs.uu.nl/download/'
 
         if not isdir(data_root):
@@ -56,7 +58,60 @@ class UMPM:
             #     utils.talk("could not find c3d file " + file, verbose)
             #     download.download_with_login(cur_url_gt, cur_loc, username, password)
 
+    def get_data(self, name):
+        """
+        :param name:
+        :return: X and C3D for the given name
+        """
+        cur_loc = join(self.data_root, name)
+        assert isdir(cur_loc)
+        video_loc = join(cur_loc, 'Video'); assert isdir(video_loc)
+        gt_loc = join(cur_loc, 'Groundtruth'); assert isdir(gt_loc)
+        shape = UMPM.get_shape(name)
+
+        # we have 4 videos: l,r,s,f
+        Videos = {'l': None, 'r': None, 's': None, 'f': None}
+        for cam in ['l', 'r', 's', 'f']:
+            fmmap = join(video_loc, name + '_' + cam + '.npy')
+            if isfile(fmmap):
+                X = np.memmap(fmmap, dtype='uint8', mode='r', shape=shape)
+            else:
+                avi = join(video_loc, name + '_' + cam + '.avi'); assert isfile(avi)
+                cap = cv2.VideoCapture(avi)
+                X = np.memmap(fmmap, dtype='uint8', mode='w+', shape=shape)
+                i = 0
+                while True:
+                    valid, frame = cap.read()
+                    if not valid:
+                        break
+                    X[i] = frame
+                    i = i + 1
+
+                del X
+                X = np.memmap(fmmap, dtype='uint8', mode='r', shape=shape)
+
+            #Videos[cam] = np.array(video)
+
+        return Videos
+
     # -------- static --------
+
+    @staticmethod
+    def get_shape(name):
+        return {
+            'p1_grab_3': (2827,486,644,3),
+            'p1_orthosyn_1': (2480,486,644,3),
+            'p1_table_2': (2866,486,644,3),
+            'p1_triangle_1': (2471,486,644,3),
+            'p2_ball_1': (2796,486,644,3),
+            'p2_chair_1': (2451,486,644,3),
+            'p2_chair_2': (2677,486,644,3),
+            'p2_circle_01': (2313,486,644,3),
+            'p2_free_1': (2437,486,644,3),
+            'p2_free_2': (2594,486,644,3),
+            'p2_grab_1': (2410,486,644,3),
+            'p2_grab_2': (2779,486,644,3)
+        }[name]
 
     @staticmethod
     def get_file_list():
