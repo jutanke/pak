361e5354ed779a9ee0b77c51d55bd72223c83c2e
Julian <jutanke@googlemail.com>
2018-03-13 17:27:28 +0100

    add UMPM

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pak",
-    version="0.0.25",
+    version="0.0.26",
     packages=[  "pak",
                 "pak/util",
                 "pak/datasets",
