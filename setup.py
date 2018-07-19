0ea2bc2a63d5279d5f1201607471b01f424b320b
Julian <jutanke@googlemail.com>
2018-07-19 12:36:17 +0200

    move bin files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pak",
-    version="0.0.35",
+    version="0.0.36",
     packages=[  "pak",
                 "pak/util",
                 "pak/datasets",
