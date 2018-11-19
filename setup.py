8faa47bc82416b20bd55590f35f3b6f4f5c58afc
Julian Tanke <jutanke@googlemail.com>
2018-11-19 13:46:24 +0100

    version +1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pak",
-    version="0.1.4",
+    version="0.1.5",
     packages=[  "pak",
                 "pak/util",
                 "pak/datasets",
