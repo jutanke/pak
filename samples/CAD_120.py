a6868ec121425f90f6392305946bc180bdde8906
jutanke <jutanke@googlemail.com>
2018-11-28 15:14:45 +0100

    cad-120 is quite noisy

--- samples/CAD_120.py
+++ samples/CAD_120.py
@@ -19,6 +19,7 @@ suj1 = data.get_subject(1)
 vid = suj1['arranging_objects']['0510175431']
 
 pts3d = data.get_3d_points_from_skel(vid['skeleton'])
+actions = vid['actions']
 
 person = pts3d[0]
 
@@ -26,13 +27,21 @@ person = pts3d[0]
 
 fig = plt.figure(figsize=(12, 12))
 ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_xlim([-800, 800])
-ax.set_ylim([-2100, -500])
-ax.set_zlim([-800, 800])
 
-data.plot(ax, person, plot_jids=True)
+n_frames = len(pts3d)
+for t in range(n_frames):
+    action = actions[t]
+    person = pts3d[t]
+    ax.clear()
+    ax.set_title(action)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_xlim([-800, 800])
+    ax.set_ylim([-2100, -500])
+    ax.set_zlim([-800, 800])
+
+    data.plot(ax, person, plot_jids=True)
+    plt.pause(33/1000)
 
 plt.show()
