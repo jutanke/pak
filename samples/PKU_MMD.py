2ffe1e066a8ffa9b76679579db8d1a7d505faabd
jutanke <jutanke@googlemail.com>
2018-12-03 18:55:17 +0100

    plotting for PKU MMD

--- samples/PKU_MMD.py
+++ samples/PKU_MMD.py
@@ -14,13 +14,12 @@ from pak.datasets.PKU_MMD import PKU_MMD, plot_skeleton
 
 data = PKU_MMD(root)
 
-skel1, _, labels1 = data.get_3d('0009-L')
-skel2, _, labels2 = data.get_3d('0009-M')
+#skel1, _, labels1 = data.get_3d('0002-L')
+skel, _, labels = data.get_3d('0002-M')
 
-fig = plt.figure(figsize=(16, 8))
+fig = plt.figure(figsize=(12, 12))
 R = 1
-ax1 = fig.add_subplot(121, projection='3d')
-ax2 = fig.add_subplot(122, projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 
 
 def plot(ax, skel):
@@ -36,14 +35,11 @@ def plot(ax, skel):
 
 
 for t in range(2000):
-    label = labels1[t]
-    plot(ax1, skel1)
-    plot(ax2, skel2)
+    label = labels[t]
 
     txt = 'None' if label == 0 else data.action_id_to_action_name[label]
-    ax1.set_title(txt)
-    ax2.set_title(txt)
-
-    plt.pause(1/33.33)
+    plot(ax, skel)
+    ax.set_title(txt)
+    plt.pause(1/66.66)
 
 plt.show()
