ec69fd3e27955da3c33af2daf7f1e593d4c7d30a
jutanke <jutanke@googlemail.com>
2018-12-04 12:35:51 +0100

    check if multi-person

--- samples/PKU_MMD.py
+++ samples/PKU_MMD.py
@@ -14,17 +14,15 @@ from pak.datasets.PKU_MMD import PKU_MMD, plot_skeleton
 
 data = PKU_MMD(root)
 
-#skel1, _, labels1 = data.get_3d('0002-L')
-skel, _, labels = data.get_3d('0002-M')
+skel1, skel2, labels = data.get_3d('0075-M')
 
 fig = plt.figure(figsize=(12, 12))
 R = 1
 ax = fig.add_subplot(111, projection='3d')
 
 
-def plot(ax, skel):
+def plot(ax, skel, t):
     s1 = skel[t]
-    ax.clear()
     ax.set_xlim(-R, R)
     ax.set_xlabel('x')
     ax.set_ylim(-2 - R * 2, 0)
@@ -34,11 +32,20 @@ def plot(ax, skel):
     plot_skeleton(ax, s1)
 
 
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+
 for t in range(2000):
     label = labels[t]
+    ax.clear()
 
     txt = 'None' if label == 0 else data.action_id_to_action_name[label]
-    plot(ax, skel)
+    plot(ax, skel1, t)
+
+    if not isclose(np.mean(np.abs(skel2)), 0):
+        plot(ax, skel2, t)
+
     ax.set_title(txt)
     plt.pause(1/66.66)
 
