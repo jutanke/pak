71856d04932bda689f81f5e1e38a5aa88d32749c
jutanke <jutanke@googlemail.com>
2019-01-08 10:06:30 +0100

    PKUMMD: group videos by person

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pak",
-    version="0.1.6",
+    version="0.1.7",
     packages=[  "pak",
                 "pak/util",
                 "pak/datasets",
