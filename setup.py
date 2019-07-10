ef4da6435d155baa2fb35cb8a670e0088889cd5c
Julian Tanke <jutanke@googlemail.com>
2019-07-10 12:30:00 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pak",
-    version="0.1.12",
+    version="0.1.13",
     packages=["pak",
               "pak/util",
               "pak/datasets",
